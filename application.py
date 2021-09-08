0d521afcfa440124811aecc6f80a0ed9957e8384
xavier-1-tech <xaviertech05@gmail.com>
2021-09-08 19:47:59 -0400

    first commit

--- application.py
+++ application.py
@@ -50,5 +50,5 @@ def index():
     return render_template("index.html", message=message)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+    #app.run(debug=True)
